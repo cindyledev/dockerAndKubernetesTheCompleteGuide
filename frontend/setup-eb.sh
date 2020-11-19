9a0b15aee1b3e431c2b037540482878d567f152e
Cindy Le <cindyledev@gmail.com>
2020-11-19 13:20:52 -0500

    add region

--- frontend/setup-eb.sh
+++ frontend/setup-eb.sh
@@ -6,6 +6,6 @@ mkdir ~/.aws
 touch ~/.aws/config
 chmod ~/.aws/config
 echo "[profile prod]" > ~/.aws/config
-echo "aws_region=$REGION" >> ~/.aws/config
+echo "default_region=$REGION" >> ~/.aws/config
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/config
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
